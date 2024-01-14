35a009f14a7720c8699204527e26a350765dcf90
ouharri <ouharrioutman@gmail.com>
2024-01-14 16:32:20 +0000

    auth routes (register, login and forgot password)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import {Routes} from '@angular/router';
+import {LoginComponent} from "./features/auth/login/login.component";
+import {RegisterComponent} from "./features/auth/register/register.component";
+import {ForgotPasswordComponent} from "./features/auth/forgot-password/forgot-password.component";
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    component: LoginComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'forgot-password', component: ForgotPasswordComponent},
+    ]
+  },
 
 ];
