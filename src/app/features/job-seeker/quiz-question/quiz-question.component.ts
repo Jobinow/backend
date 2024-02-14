9f475abb88e54021f8684c2547bd33d67795e2b2
ouharri <ouharrioutman@gmail.com>
2024-02-14 11:26:43 +0000

    up up

--- src/app/features/job-seeker/quiz-question/quiz-question.component.ts
+++ src/app/features/job-seeker/quiz-question/quiz-question.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {TimerComponent} from "../../../shared/timer/timer.component";
+import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-quiz-question',
   standalone: true,
-  imports: [],
+  imports: [
+    TimerComponent,
+    RouterLink,
+    NgOptimizedImage
+  ],
   templateUrl: './quiz-question.component.html',
   styleUrl: './quiz-question.component.css'
 })
